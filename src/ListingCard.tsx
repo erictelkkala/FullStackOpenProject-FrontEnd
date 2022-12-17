170f71c30b724e40edae53bafc94fbea86489cf4
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-12-17 13:45:22 +0200

    Added Prettier and ESLint

--- src/ListingCard.tsx
+++ src/ListingCard.tsx
@@ -5,11 +5,10 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
+
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
+
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
