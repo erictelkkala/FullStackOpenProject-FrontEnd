7e2cbda9a9ec6bf52450aa590ae50bfd706f9fe4
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2022-12-17 12:05:14 +0200

    Initial theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { AppBar, Box, CssBaseline, ThemeProvider, Toolbar, Typography } from '@mui/material'
+import { appTheme } from './themes/main'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ThemeProvider theme={appTheme}>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Album layout
+            </Typography>
+          </Toolbar>
+      </AppBar>
+    </Box>
+  </ThemeProvider>
   )
 }
 
---
+++ src/themes/main.ts
@@ -0,0 +1,30 @@
+import { PaletteMode } from "@mui/material";
+import { createTheme, PaletteColorOptions } from "@mui/material/styles";
+import { ColorPartial, CommonColors, PaletteTonalOffset, TypeAction, TypeBackground, TypeText } from "@mui/material/styles/createPalette";
+
+declare module '@mui/material/styles' {
+    interface PaletteOptions {
+        primary?: PaletteColorOptions;
+        secondary?: PaletteColorOptions;
+        error?: PaletteColorOptions;
+        warning?: PaletteColorOptions;
+        info?: PaletteColorOptions;
+        success?: PaletteColorOptions;
+        mode?: PaletteMode;
+        tonalOffset?: PaletteTonalOffset;
+        contrastThreshold?: number;
+        common?: Partial<CommonColors>;
+        grey?: ColorPartial;
+        text?: Partial<TypeText>;
+        divider?: string;
+        action?: Partial<TypeAction>;
+        background?: Partial<TypeBackground>;
+        getContrastText?: (background: string) => string;
+    }
+}
+
+export const appTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
