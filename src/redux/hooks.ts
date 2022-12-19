70e72700f7df7dff6fbfc24f2fde481dc47ce205
erictelkkala <eric.telkkala@gmail.com>
2022-12-19 21:38:46 +0200

    New hook to fetch the content of the cart

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -4,3 +4,10 @@ import type { RootState, AppDispatch } from './store'
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useCart = () => {
+  const cart = useAppSelector((state) => state.shoppingCart.items)
+
+  return {
+    cart
+  }
+}
