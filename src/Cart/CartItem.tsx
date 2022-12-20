82fdbea86d70c0caa0479001976a687c51e1376a
erictelkkala <eric.telkkala@gmail.com>
2022-12-20 16:57:12 +0200

    Dialog message for deleting the item from the cart

--- src/Cart/CartItem.tsx
+++ src/Cart/CartItem.tsx
@@ -6,7 +6,12 @@ import {
   Box,
   Button,
   FormControl,
-  IconButton
+  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
 } from '@mui/material'
 
 import RemoveIcon from '@mui/icons-material/Remove'
@@ -14,46 +19,80 @@ import AddIcon from '@mui/icons-material/Add'
 
 import { Item } from '../types'
 
+import React from 'react'
+
 function CartItem(item: Item) {
+  const [open, setOpen] = React.useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
   return (
-    <Card raised sx={{ display: 'flex' }}>
-      <CardMedia
-        component="img"
-        sx={{ width: 200 }}
-        // FIXME: temporary image
-        image="https://material-ui.com/static/images/cards/live-from-space.jpg"
-        alt="Live from space album cover"
-      />
+    <Box>
+      <Card raised sx={{ display: 'flex' }}>
+        <CardMedia
+          component="img"
+          sx={{ width: 200 }}
+          // FIXME: temporary image
+          image="https://material-ui.com/static/images/cards/live-from-space.jpg"
+          alt="Live from space album cover"
+        />
 
-      {/* Title of the item and the quantity below it */}
-      <CardContent sx={{ display: 'flex', flexDirection: 'column', marginRight: 'auto' }}>
-        <Typography variant="h5">{item.name}</Typography>
-        <FormControl sx={{ flexDirection: 'row' }}>
-          <IconButton>
-            <RemoveIcon />
-          </IconButton>
-          <Typography variant="subtitle1" sx={{ alignSelf: 'center' }}>
-            {item.quantity}
-          </Typography>
-          <IconButton>
-            <AddIcon />
-          </IconButton>
-        </FormControl>
-      </CardContent>
+        {/* Title of the item and the quantity below it */}
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', marginRight: 'auto' }}>
+          <Typography variant="h5">{item.name}</Typography>
+          <FormControl sx={{ flexDirection: 'row' }}>
+            <IconButton>
+              <RemoveIcon />
+            </IconButton>
+            <Typography variant="subtitle1" sx={{ alignSelf: 'center' }}>
+              {item.quantity}
+            </Typography>
+            <IconButton>
+              <AddIcon />
+            </IconButton>
+          </FormControl>
+        </CardContent>
 
-      {/* Price of the item and the delete button */}
-      <CardContent sx={{ display: 'flex', flexDirection: 'column' }}>
-        {/* MarginBottom to make the delete button hit the "floor" of the box */}
-        <Box sx={{ display: 'flex', justifyContent: 'end', marginBottom: 'auto' }}>
-          <Typography variant="h5">{item.price} €</Typography>
-        </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Button variant="contained" color="error">
-            Delete
+        {/* Price of the item and the delete button */}
+        <CardContent sx={{ display: 'flex', flexDirection: 'column' }}>
+          {/* MarginBottom to make the delete button hit the "floor" of the box */}
+          <Box sx={{ display: 'flex', justifyContent: 'end', marginBottom: 'auto' }}>
+            <Typography variant="h5">{item.price} €</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Button variant="contained" color="error" onClick={handleClickOpen}>
+              Delete
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
+
+      {/* Dialog for deleting the item from the cart */}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'Delete item?'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure that you want to delete this item?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>No</Button>
+          <Button onClick={handleClose} autoFocus>
+            Yes
           </Button>
-        </Box>
-      </CardContent>
-    </Card>
+        </DialogActions>
+      </Dialog>
+    </Box>
   )
 }
 
