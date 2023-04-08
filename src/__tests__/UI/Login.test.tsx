1015ef045c66d5144f95f2b3e26721250c3a7f8d
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2023-04-08 11:35:39 +0300

    test(Login): add test for login behaviour

--- src/__tests__/UI/Login.test.tsx
+++ src/__tests__/UI/Login.test.tsx
@@ -1,6 +1,8 @@
 import { screen } from '@testing-library/react'
 import Login from '../../Login/Login'
 import { render } from '../../utils/test-utils.js'
+import userEvent from '@testing-library/user-event'
+import { vi } from 'vitest'
 
 describe('Login', () => {
   it('renders Login component', () => {
@@ -12,7 +14,44 @@ describe('Login', () => {
     render(<Login />)
     expect(screen.getByLabelText('Username')).toBeInTheDocument()
     expect(screen.getByLabelText('Password')).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /signup/i })).toHaveTextContent('Signup')
     expect(screen.getByRole('button', { name: /login/i })).toHaveTextContent('Login')
     expect(screen.getByRole('button', { name: /login/i })).toBeDisabled()
+  })
+  it('renders the Login form with errors', async () => {
+    const user = userEvent.setup()
+    const mockSubmit = vi.fn()
+
+    render(<Login onSubmit={mockSubmit} />)
+
+    const loginButton = screen.getByRole('button', { name: /login/i })
+    const usernameField = screen.getByLabelText('Username')
+    const passwordField = screen.getByLabelText('Password')
+
+    expect(loginButton).toBeDisabled()
+    expect(loginButton).toHaveTextContent('Login')
+
+    expect(usernameField).toHaveTextContent('')
+    expect(passwordField).toHaveTextContent('')
+
+    await user.type(usernameField, 'test')
+    expect(loginButton).toBeDisabled()
+
+    // Password too short
+    await user.type(passwordField, 'tes')
+    expect(loginButton).toBeDisabled()
+
+    // Password too long
+    await user.clear(passwordField)
+    await user.type(
+      passwordField,
+      'asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd'
+    )
+    expect(loginButton).toBeDisabled()
+
+    // Password correct length
+    await user.clear(passwordField)
+    await user.type(passwordField, 'test')
+    expect(loginButton).toBeEnabled()
   })
 })
