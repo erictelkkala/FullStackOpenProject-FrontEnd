28f5567d8ac70b91182c1189d39cc1561f50c684
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2023-04-09 14:40:02 +0300

    build: use chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
-import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,13 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    minify: true
+    minify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    }
   }
 })
