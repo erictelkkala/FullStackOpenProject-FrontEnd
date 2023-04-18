f9b3d05606f1deae6d6a8a577baeac56f4740e3b
erictelkkala <eric.telkkala@gmail.com>
2023-04-18 21:29:05 +0300

    feat(Cart): cart now handles routing to checkout
    - Cart handles navigating to checkout
    - Bottom bar for showing the total amount and button for navigating to checkout

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 
-import { Chip, Container, Divider, Stack, Typography } from '@mui/material'
+import { AppBar, Chip, Container, Divider, Stack, Toolbar, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 
 import { useCartItems } from '../redux/hooks'
@@ -10,8 +10,12 @@ function Cart() {
   const items = useCartItems()
   const navigate = useNavigate()
 
-  const handleClick = () => {
+  const handleEmptyCart = () => {
     navigate('/')
+  }
+
+  const handleCheckout = () => {
+    navigate('/checkout')
   }
 
   return (
@@ -24,11 +28,31 @@ function Cart() {
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
               <Typography variant="h4">Your cart is empty</Typography>
             </Box>
-            <Divider sx={{ paddingTop: 5 }}>
-              <Chip label="Return to Home" color="primary" variant="filled" onClick={handleClick} />
+            <Divider sx={{ pt: 5 }}>
+              <Chip
+                label="Return to Home"
+                color="primary"
+                variant="filled"
+                onClick={handleEmptyCart}
+              />
             </Divider>
           </>
         )}
+        <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }} aria-label="navbar">
+          <Toolbar>
+            <Typography variant="h5" color="inherit" aria-label="bottombar-total-amount">
+              Total:{' '}
+              {items.reduce((prevValue, currValue) => prevValue + currValue.listing_price, 0)} €
+            </Typography>
+            {/* Button on the right */}
+            <Box sx={{ flexGrow: 1 }} />
+            {items.length > 0 ? (
+              <Chip label="Checkout" color="primary" variant="filled" onClick={handleCheckout} />
+            ) : (
+              <Chip disabled label="Checkout" color="primary" variant="filled" />
+            )}
+          </Toolbar>
+        </AppBar>
       </Stack>
     </Container>
   )
