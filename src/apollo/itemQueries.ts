08152d738dc827aaaa29e9dd9b660bcb70c301f1
erictelkkala <eric.telkkala@gmail.com>
2023-04-24 20:47:24 +0300

    fix(apollo): :bug: modified queries to proper format

--- src/apollo/itemQueries.ts
+++ src/apollo/itemQueries.ts
@@ -1,63 +1,65 @@
-export const GET_ITEMS = `#graphql
-    query allItems {
-        items {
-            id
-            listing_title
-            listing_description
-            listing_price
-            listing_image
-            listing_category
-            listing_quantity
-        }
+import { gql } from '@apollo/client'
+
+export const GET_ITEMS = gql`
+  query allItems {
+    items {
+      id
+      listing_title
+      listing_description
+      listing_price
+      listing_image
+      listing_category
+      listing_quantity
     }
+  }
 `
 
-export const ADD_ITEM = `#graphql
-    mutation addItem(
-        $listing_title: String!
-        $listing_description: String!
-        $listing_price: Float!
-        $listing_image: String!
-        $listing_category: Categories!
-        $listing_quantity: Int!
+export const ADD_ITEM = gql`
+  mutation addItem(
+    $listing_title: String!
+    $listing_description: String!
+    $listing_price: Float!
+    $listing_image: String!
+    $listing_category: Categories!
+    $listing_quantity: Int!
+  ) {
+    addItem(
+      listing_title: $listing_title
+      listing_description: $listing_description
+      listing_price: $listing_price
+      listing_image: $listing_image
+      listing_category: $listing_category
+      listing_quantity: $listing_quantity
     ) {
-        addItem(
-            listing_title: $listing_title
-            listing_description: $listing_description
-            listing_price: $listing_price
-            listing_image: $listing_image
-            listing_category: $listing_category
-            listing_quantity: $listing_quantity
-        ) {
-            id
-            listing_title
-            listing_description
-            listing_price
-            listing_image
-            listing_category
-            listing_quantity
-        }
+      id
+      listing_title
+      listing_description
+      listing_price
+      listing_image
+      listing_category
+      listing_quantity
     }
+  }
 `
 
-export const DELETE_ITEM = `#graphql
-    mutation deleteItem($id: ID!) {
-        deleteItem(id: $id) {
-            id
-        }
+export const DELETE_ITEM = gql`
+  mutation deleteItem($id: ID!) {
+    deleteItem(id: $id) {
+      id
     }
+  }
 `
 
-export const GET_ITEM = `#graphql
-    query getItem($id: ID!) {
-        item(id: $id) {
-            id
-            listing_title
-            listing_description
-            listing_price
-            listing_image
-            listing_category
-            listing_quantity
-        }
+export const GET_ITEM = gql`
+  query getItem($id: ID!) {
+    item(id: $id) {
+      id
+      listing_title
+      listing_description
+      listing_price
+      listing_image
+      listing_category
+      listing_quantity
     }
+  }
 `
--- src/apollo/userQueries.ts
+++ src/apollo/userQueries.ts
@@ -1,26 +1,28 @@
-export const GET_USER = `#graphql
-    query findUserById($id: ID!) {
-        user(id: $id) {
-            _id
-            name
-        }
+import { gql } from '@apollo/client'
+
+export const GET_USER = gql`
+  query findUserById($id: ID!) {
+    user(id: $id) {
+      _id
+      name
     }
+  }
 `
 
-export const ADD_USER = `#graphql
-    mutation addUser($name: String!, $password: String!) {
-        addUser(name: $name, password: $password) {
-            _id
-            name
-        }
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $password: String!) {
+    addUser(name: $name, password: $password) {
+      _id
+      name
     }
+  }
 `
 
-export const LOGIN_USER = `#graphql
-    mutation login($name: String!, $password: String!) {
-        login(name: $name, password: $password) {
-            _id
-            name
-        }
+export const LOGIN_USER = gql`
+  mutation login($name: String!, $password: String!) {
+    login(name: $name, password: $password) {
+      _id
+      name
     }
+  }
 `
