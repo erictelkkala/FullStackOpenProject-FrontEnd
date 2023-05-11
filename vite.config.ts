fdbb3d808a17be16a708fa0222d35a0c8ac7687e
erictelkkala <eric.telkkala@gmail.com>
2023-05-12 00:03:21 +0300

    build: :bug: added new packages for Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 import { defineConfig } from 'vite'
 
+import image from '@rollup/plugin-image'
+import typescript from '@rollup/plugin-typescript'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), typescript(), image()],
   build: {
     target: 'esnext',
     minify: true,
+    sourcemap: true,
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom']
+          react: ['react', 'react-dom', 'react-router-dom'],
+          apollo: ['@apollo/client', 'graphql']
+        },
+        generatedCode: {
+          constBindings: true
         }
       }
     }
