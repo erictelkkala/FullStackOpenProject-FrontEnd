7ef8696a93177e6b7d5b7a4b1b58d122eadb60c6
erictelkkala <eric.telkkala@gmail.com>
2023-06-17 20:02:30 +0300

    test(CheckoutItem): :white_check_mark: new tests for CheckoutItem
    - Ensure that quantity is rendered with other quantities than 1
    - displays itself in an error color if quantity is 0

--- src/__tests__/UI/Checkout/CheckoutItem.test.tsx
+++ src/__tests__/UI/Checkout/CheckoutItem.test.tsx
@@ -1,4 +1,4 @@
-import { describe } from 'vitest'
+import { describe, it } from 'vitest'
 
 import { screen } from '@testing-library/react'
 
@@ -34,8 +34,49 @@ describe('CheckoutItem', () => {
       'src',
       'src\\assets\\react.svg'
     )
-    expect(
-      screen.getByRole('heading', { name: 'Quantity and price of the item' })
-    ).toHaveTextContent('1 x 100 €')
+    expect(screen.getByRole('heading', { name: 'Quantity of the item' })).toHaveTextContent('1')
+    expect(screen.getByRole('heading', { name: 'x' })).toHaveTextContent('x')
+    expect(screen.getByRole('heading', { name: 'Price of the item' })).toHaveTextContent('100 €')
+  })
+
+  it('renders the item with another quantity than 1', () => {
+    render(<CheckoutItem {...item} />, {
+      preloadedState: {
+        shoppingCart: {
+          items: [item],
+          quantity: [{ id: '1', quantity: 2 }]
+        }
+      }
+    })
+
+    expect(screen.getByRole('heading', { name: 'Name of the item' })).toHaveTextContent(
+      'Cart item title'
+    )
+    // Text content shouldn't be 1 x 100 €, but 2 x 100 €
+    expect(screen.getByRole('heading', { name: 'Quantity of the item' })).not.toHaveTextContent('1')
+    expect(screen.getByRole('heading', { name: 'x' })).toHaveTextContent('x')
+    expect(screen.getByRole('heading', { name: 'Quantity of the item' })).toHaveTextContent('2')
+  })
+
+  it('renders the quantity with an error color if the quantity is 0', () => {
+    render(<CheckoutItem {...item} />, {
+      preloadedState: {
+        shoppingCart: {
+          items: [item],
+          quantity: [{ id: '1', quantity: 0 }]
+        }
+      }
+    })
+
+    expect(screen.getByRole('heading', { name: 'Name of the item' })).toHaveTextContent(
+      'Cart item title'
+    )
+    expect(screen.getByRole('heading', { name: 'Quantity of the item' })).toHaveTextContent('0')
+    expect(screen.getByRole('heading', { name: 'x' })).toHaveTextContent('x')
+    expect(screen.getByRole('heading', { name: 'Price of the item' })).toHaveTextContent('100 €')
+    // The color of the quantity should be error
+    expect(screen.getByRole('heading', { name: 'Quantity of the item' })).toHaveStyle({
+      color: 'rgb(244, 67, 54)'
+    })
   })
 })
